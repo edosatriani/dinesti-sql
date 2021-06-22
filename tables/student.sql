19734e46adb717fd008afc4f9437fdf5c72ec5fe
Kurnia Hidayat <edo.satriani@gmail.com>
2021-06-22 20:07:34 +0700

    add student table

---
+++ tables/student.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `student` (
+  `uuid` varchar(32) NOT NULL DEFAULT uuid(),
+  `autonumber` int(11) NOT NULL,
+  `student_code` varchar(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,  
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `created_by` varchar(50) NOT NULL,
+  `created_ts` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(50) NOT NULL,
+  `updated_ts` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`autonumber`);
+
+ALTER TABLE `student`
+  MODIFY `autonumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
