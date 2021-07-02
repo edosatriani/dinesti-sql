70dd3ad611abe13e0198da5fa657deb8e1b107f2
habsyi-ikhlashulariq <habsyi.ikhlashulariq@gmail.com>
2021-07-02 23:36:02 +0700

    add user table

---
+++ tables/user.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `user` (
+  `uuid` varchar(32) NOT NULL DEFAULT uuid(),
+  `autonumber` int(11) NOT NULL,
+  `code` varchar(6) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `user_role_id` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `created_by` varchar(50) NOT NULL,
+  `created_ts` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(50) NOT NULL,
+  `updated_ts` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`autonumber`);
+
+ALTER TABLE `user`
+  MODIFY `autonumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
