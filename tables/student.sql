897c658a55aec17f5ab33361907fc33240dc3691
habsyi-ikhlashulariq <habsyi.ikhlashulariq@gmail.com>
2021-07-05 09:22:01 +0700

    update student table

--- tables/student.sql
+++ tables/student.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `student` (
   `uuid` varchar(32) NOT NULL DEFAULT uuid(),
   `autonumber` int(11) NOT NULL,
-  `student_code` varchar(10) NOT NULL,
+  `code` varchar(10) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `date_birth` date NOT NULL,
+  `phone_number` varchar(14) NOT NULL,
   `address` varchar(100) NOT NULL,  
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `created_by` varchar(50) NOT NULL,
