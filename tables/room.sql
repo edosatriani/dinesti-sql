7ffd174e3c499e77eb172fd8a4684096b3cd022c
habsyi-ikhlashulariq <habsyi.ikhlashulariq@gmail.com>
2021-07-05 14:50:05 +0700

    update room table

--- tables/room.sql
+++ tables/room.sql
@@ -3,6 +3,7 @@ CREATE TABLE `room` (
   `autonumber` int(11) NOT NULL,
   `code` varchar(10) NOT NULL,
   `description` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
   `created_by` varchar(50) NOT NULL,
   `created_ts` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_by` varchar(50) NOT NULL,
