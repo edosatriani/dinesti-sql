e978e2849c1d6246cbb682a535bfea1e9622d565
Kurnia Hidayat <edo.satriani@gmail.com>
2021-07-22 19:15:05 +0700

    add course_registration

---
+++ tables/course_registration.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `course_registration` (
+  `uuid` varchar(32) NOT NULL DEFAULT uuid(),
+  `autonumber` int(11) NOT NULL,
+  `registration_no` varchar(100) NOT NULL,  
+  `registration_date` date NOT NULL,
+  `student_id` varchar(10) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `created_by` varchar(50) NOT NULL,
+  `created_ts` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_by` varchar(50) NOT NULL,
+  `updated_ts` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE `course_registration`
+  ADD PRIMARY KEY (`autonumber`);
+
+ALTER TABLE `course_registration`
+  MODIFY `autonumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
