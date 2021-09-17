f97ea72d9260f3a7f05b141ba3e068e4bb52aee3
Habsyi Ikhlashul Ariq <64714475+habsyi-ikhlashulariq@users.noreply.github.com>
2021-09-17 15:11:39 +0700

    Update grade.sql

--- tables/grade.sql
+++ tables/grade.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `grade` (
   `uuid` varchar(32) NOT NULL DEFAULT uuid(),
   `autonumber` int(11) NOT NULL,
-  `code` varchar(2) NOT NULL,
+  `code` varchar(10) NOT NULL,
   `description` varchar(20) NOT NULL,
+  `is_concert` tinyint(4) NOT NULL DEFAULT 0,
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `created_by` varchar(50) NOT NULL,
   `created_ts` timestamp NOT NULL DEFAULT current_timestamp(),
