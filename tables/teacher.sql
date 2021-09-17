35dcc2e99a807cde4d6167bb4201798a5fe45b51
Habsyi Ikhlashul Ariq <64714475+habsyi-ikhlashulariq@users.noreply.github.com>
2021-09-17 15:23:04 +0700

    Update teacher.sql

--- tables/teacher.sql
+++ tables/teacher.sql
@@ -4,6 +4,7 @@ CREATE TABLE `teacher` (
   `code` varchar(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `phone_number` varchar(13) NOT NULL,
+  `concert_specialist` int(11) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `created_by` varchar(50) NOT NULL,
   `created_ts` timestamp NOT NULL DEFAULT current_timestamp(),
